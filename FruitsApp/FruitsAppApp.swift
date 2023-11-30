9a115ed83930820cb3099af2564aac1b34cd1f26
saiprasad <saiprasadrekha881@gmail.com>
2023-11-30 15:26:48 +0530

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Sai Prasad Rekha on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
