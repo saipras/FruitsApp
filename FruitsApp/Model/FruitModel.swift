bf0f5dac16da4209d68aab389f1dff8a4b6945bc
saiprasad <saiprasadrekha881@gmail.com>
2023-11-30 15:39:47 +0530

    added new features

---
+++ FruitsApp/Model/FruitModel.swift
@@ -0,0 +1,21 @@
+//
+//  FruitModel.swift
+//  FruitsApp
+//
+//  Created by Sai Prasad Rekha on 30/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+  var id = UUID()
+  var title: String
+  var headline: String
+  var image: String
+  var gradientColors: [Color]
+  var description: String
+  var nutrition: [String]
+}
